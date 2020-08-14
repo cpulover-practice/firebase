d1d095882af3bea658d1e5a6ad0f24b70e8108f6
cpulover <justacpulover@gmail.com>
2020-08-14 13:29:11 +0200

    SETUP: Reset project

---
+++ public/js/app.js
@@ -0,0 +1,17 @@
+var authRef = firebase.auth();
+var functionRef = firebase.functions();
+var firestoreRef = firebase.firestore();
+var databaseRef = firebase.database();
+var storageRef = firebase.storage();
+
+// console.log(authRef);
+// console.log(functionRef);
+// console.log(firestoreRef);
+// console.log(databaseRef);
+// console.log(storageRef);
+// console.log(firebase);
+
+
+
+
+
